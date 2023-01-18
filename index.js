812e6eaedd89f26c32f239c31af84744a89a0940
robert5313 <kiptoorobert@etech.ac.ke>
2023-01-18 13:23:36 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const dotenv = require('dotenv').config();
+const port = process.env.PORT || 5000;
+
+const app = express();
+
+//enable body parser
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use('/openai', require('./routes/OpenaiRoutes'));
+
+app.listen(port, () => console.log(`server started on port ${port}`));
